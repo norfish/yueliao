f132f13ec803e8f65d6cd2bf8226e497e3a27800
yongxiang.li <yongxiang.li@qunar.com>
2013-11-01 16:06:14 +0800

    router

--- routes/index.js
+++ routes/index.js
@@ -1,40 +1,51 @@
 'use strict';
 
 var regist = require('./reg'); 
+var login = require('./login');
 
 module.exports = function(app){
 	app.get('/', function (req, res){
 		req.flash('info', 'hi there');
-		res.render('index', { title: '主页' });
+		res.render('index', { title: '主页' ,
+							error: req.flash('error'),
+							success: req.flash('success')
+		});
 	});
-
+	
 	app.get('/reg', function (req, res){
-		res.render('reg', { title: '注册',
-												message: req.flash('error')
-						});
+		res.render('reg', { title: '注册', message: req.flash('error') });
 	});
 
 	app.post('/reg', function (req, res){
 		regist(req, res);
 	});
 
+	app.get('/login', checkNotLogin);
 	app.get('/login', function (req, res){
-		res.render('login', { title: '登录' });
+		console.log('get')
+		res.render('login', { title: '登录', message: req.flash('error')});
 	});
 
+	app.post('/login', checkNotLogin);
 	app.post('/login', function (req, res){
+		console.log('login')
+		login(req, res);
 	});
 
+	app.get('/post', checkLogin);
 	app.get('/post', function (req, res){
 		res.render('post', { title: '发表' });
 	});
 
+	app.post('/post', checkLogin);
 	app.post('/post', function (req, res){
 
 	});
 
 	app.get('/logout', function (req, res){
-		
+		req.session.user = null;
+		req.flash('success', '退出成功');
+		res.redirect('/');
 	});
 
 	app.get('/blog', function (req, res){
@@ -44,3 +55,19 @@ module.exports = function(app){
 	app.post('/blog', function (req, res){
 	})
 };
+
+function checkLogin(req, res, next) {
+  if (!req.session.user) {
+    req.flash('error', '未登录!'); 
+    res.redirect('/login');
+  }
+  next();
+}
+
+function checkNotLogin(req, res, next) {
+  if (req.session.user) {
+    req.flash('error', '已登录!'); 
+    res.redirect('back');
+  }
+  next();
+}
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,40 @@
-
+var crypto = require('crypto');
+var User   = require('../models/user');
 /*
  * GET home page.
  */
+module.exports = login;
 
-exports.login = function(req, res){
-  res.render( 'login', {title: '注册'} );
+function login(req, res){
+	//res.render( 'login', {title: '注册'} );
+	//console.log("123")
+	var name = req.body.name,
+		password = req.body.password;
+	console.log(password);
+	var md5 = crypto.createHash('md5'),
+		password = md5.update(password).digest('hex');
+
+	if(name === "" || password === ""){
+		console.log("err")
+		req.flash('error', '用户名或密码不能为空！');
+		return res.redirect('/login');
+	}
+
+	User.get(name, function(err, user){
+		if(!user){
+			req.flash('error', '用户名不存在');
+			return res.redirect('/login');
+		}
+
+		if(user.password !== password){
+			req.flash('error', '密码不正确');
+			return res.redirect('/login');
+		}
+
+		req.session.user = user;
+		req.flash('success', '登录成功');
+		res.redirect('/');
+	})
+
+
 };
